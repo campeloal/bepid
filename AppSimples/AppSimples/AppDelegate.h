22624388348444577aadc229cba59022f7109ab0
Alex De Souza Campelo Lima <alcampelo@Alexs-MacBook-Pro.local>
2014-05-13 13:56:32 -0300

    app simples - first app

---
+++ AppSimples/AppSimples/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AppSimples
+//
+//  Created by Teste on 12/05/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ AppSimples/ITViewController.h
@@ -0,0 +1,30 @@
+//
+//  ITViewController.h
+//  AppSimples
+//
+//  Created by Teste on 12/05/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITViewController : UIViewController
+{
+    IBOutlet UITextField *txtIdade;
+    IBOutlet UITextField *txtTel;
+    IBOutlet UITextField *envSucesso;
+    IBOutlet UISlider *slider;
+}
+
+@property(nonatomic,retain) IBOutlet UITextField *txtIdade;
+@property(nonatomic,retain) IBOutlet UITextField *envSucesso;
+@property(nonatomic,retain) IBOutlet UISlider *slider;
+@property(nonatomic,retain) IBOutlet UITextField *txtTel;
+
+- (IBAction) updateAge:(id) sender;
+- (IBAction) saveData:(id)sender;
+- (BOOL) checkEnv;
+- (NSString*) saveFilePath;
+-(void) dismissKeyboard;
+
+@end
